91708d56cbc7bd18066c4bd2fda0df49a041240a
Ajith Arasu <mail2ajith2405@gmail.com>
2025-10-15 16:40:09 +0530

    add husky and lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,52 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import json from "@eslint/json";
-import pluginPrettier from "eslint-plugin-prettier";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import json from '@eslint/json';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  // JS / JSX files
+  // JS / JSX files in src
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['src/**/*.{js,jsx}'],
     plugins: { js, prettier: pluginPrettier },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
     rules: {
-      "prettier/prettier": "error"
-    }
+      'prettier/prettier': 'error',
+    },
   },
 
-  // React rules only for JS / JSX
+  // React rules only for JS / JSX in src
   {
-    files: ["**/*.{js,jsx}"],
-    ...pluginReact.configs.flat.recommended
+    files: ['src/**/*.{js,jsx}'],
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // disable "React must be in scope"
+    },
   },
 
-  // JSON files
+  // JSON files anywhere (optional, you can limit to src if needed)
   {
-    files: ["**/*.json"],
+    files: ['src/**/*.json'],
     plugins: { json, prettier: pluginPrettier },
-    language: "json/json",
-    extends: ["json/recommended"],
-    rules: { "prettier/prettier": "error" }
+    language: 'json/json',
+    extends: ['json/recommended'],
+    rules: { 'prettier/prettier': 'error' },
   },
   {
-    files: ["**/*.jsonc"],
+    files: ['src/**/*.jsonc'],
     plugins: { json, prettier: pluginPrettier },
-    language: "json/jsonc",
-    extends: ["json/recommended"],
-    rules: { "prettier/prettier": "error" }
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+    rules: { 'prettier/prettier': 'error' },
   },
   {
-    files: ["**/*.json5"],
+    files: ['src/**/*.json5'],
     plugins: { json, prettier: pluginPrettier },
-    language: "json/json5",
-    extends: ["json/recommended"],
-    rules: { "prettier/prettier": "error" }
-  }
+    language: 'json/json5',
+    extends: ['json/recommended'],
+    rules: { 'prettier/prettier': 'error' },
+  },
 ]);
