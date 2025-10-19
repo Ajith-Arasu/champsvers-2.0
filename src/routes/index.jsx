48e0bc209267a67b377780cee30f4688737b6ebd
Ajith Arasu <mail2ajith2405@gmail.com>
2025-10-19 08:34:28 +0530

    update intro card page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../App';
 import Home from '../pages/Home';
 import LatestWork from '../pages/LatestWork';
-
+import IntroCard from '../pages/introCard';
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<IntroCard />} />
         <Route element={<App />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Home />} />
           <Route path="/latestwork" element={<LatestWork />} />
         </Route>
       </Routes>
