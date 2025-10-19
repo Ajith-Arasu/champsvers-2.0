66e636157103609926e80d73ff37b4cbfc63c755
Ajith Arasu <mail2ajith2405@gmail.com>
2025-10-19 15:23:47 +0530

    update navbar and dropdown

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,7 +5,18 @@ const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <img src={logo} alt="Logo" className={styles.logo} />
-      <div className={styles.user}>Shishya 🐼</div>
+
+      <div className={styles.userWrapper}>
+        <div className={styles.userTrigger}>
+          <img src="https://icotar.com/avatar/craig" alt="Avatar" className={styles.avatar} />
+          <span className={styles.username}>Shishya</span>
+        </div>
+
+        <div className={styles.dropdown}>
+          <div className={styles.dropdownItem}>Profile</div>
+          <div className={styles.dropdownItem}>Logout</div>
+        </div>
+      </div>
     </div>
   );
 };
