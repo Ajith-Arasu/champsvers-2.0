5d38419552706bfbef1e0d669b931b4a54a4695a
nithyanandhini375 <nithyanandhini375@gmail.com>
2025-10-21 13:03:05 +0530

    Home page UI

---
+++ src/components/Homepage/ShowCaseStatCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './style.module.css';
+
+const ShowCaseStatCard = () => {
+  return (
+    <div className={styles.showcase_statcard}>
+      <p>VIEW MORE STATS</p>
+    </div>
+  )
+}
+
+export default ShowCaseStatCard
