53e0a5f30a8ee4ef2c7369bfe7cab4738a0cf373
nithyanandhini375 <nithyanandhini375@gmail.com>
2025-10-23 15:42:15 +0530

    changes done

--- src/components/Homepage/ShowCaseCard.jsx
+++ src/components/Homepage/ShowCaseCard.jsx
@@ -4,7 +4,7 @@ import styles from './style.module.css';
 const ShowCaseCard = (props) => {
   return (
     <div className={styles.showcase_card}>
-      <p className={styles.card_value}>{props.value}</p>
+      <span className={styles.card_value}>{props.value}</span>
       <span className={styles.card_text}>{props.text}</span>
     </div>
   )
