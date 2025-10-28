b1e6d6368aa95b31c21e55a36240bc9f8744fc50
nithyanandhini375 <nithyanandhini375@gmail.com>
2025-10-28 16:55:23 +0530

    changes made

--- src/pages/Students/studentdetails.jsx
+++ src/pages/Students/studentdetails.jsx
@@ -1,11 +1,6 @@
 import styles from './style.module.css';
 import userimg from '../../assets/images/Boy.png';
-import img1 from '../../assets/images/image1.png';
-import img2 from '../../assets/images/image2.png';
-import img3 from '../../assets/images/image3.png';
-import img4 from '../../assets/images/image4.png';
-import img5 from '../../assets/images/image5.png';
-import img6 from '../../assets/images/image6.png';
+import StudentCreations from '../../components/Studentcreations/StudentCreations';
 
 const StudentDetails = () => {
   return( 
@@ -86,14 +81,7 @@ const StudentDetails = () => {
             <span className={styles.info_label}>TROPHIES</span>
           </div> 
         </div>
-        <div className={styles.student_creations}>
-          <img className={styles.imagegallery} src={img1} alt="studentcreationimage" />
-          <img className={styles.imagegallery} src={img2} alt="studentcreationimage" />
-          <img className={styles.imagegallery} src={img3} alt="studentcreationimage" />
-          <img className={styles.imagegallery} src={img4} alt="studentcreationimage" />
-          <img className={styles.imagegallery} src={img5} alt="studentcreationimage" />
-          <img className={styles.imagegallery} src={img6} alt="studentcreationimage" />
-        </div>
+        <StudentCreations/>
       </div>
     </div>
   );
