62475cd8b759cbf12caa0658fda67f12e3d0a940
nithyanandhini375 <nithyanandhini375@gmail.com>
2025-10-29 17:30:43 +0530

    latestwork

--- src/pages/LatestWorks/index.jsx
+++ src/pages/LatestWorks/index.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import styles from './styles.module.css';
+import StudentCreations from '../../components/Studentcreations/StudentCreations';
+
 const LatestWork = () => {
-  return <h1>Latest Work</h1>;
+  return (
+    <div className={styles.latestwork}>
+      <div className={styles.title}>Latest Works</div>
+      <div className={styles.works_gallery}>
+        <StudentCreations />
+        <StudentCreations />
+        <StudentCreations />
+      </div>
+    </div>
+  )
 };
 
 export default LatestWork;
