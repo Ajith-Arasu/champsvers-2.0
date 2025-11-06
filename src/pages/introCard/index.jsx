5d3ee5a8afbe0b1a9f32fce7e4e36c142508d57a
nithyanandhini375 <nithyanandhini375@gmail.com>
2025-11-06 17:28:21 +0530

    Introcard UI changes

--- src/pages/introCard/index.jsx
+++ src/pages/introCard/index.jsx
@@ -1,18 +1,32 @@
 import { Link } from 'react-router-dom';
 import styles from './style.module.css';
 import logo from '../../assets/images/cvlogo.png';
+import Button from '../../components/Button/Button';
 
 const IntroCard = () => {
   return (
     <div className={styles.background}>
-      <div className={styles.overlayBox}>
-        <h1 className={styles.title}>
-          Schools, <span className={styles.gamified}>Gamified.</span>
-        </h1>
-
-        <Link to="/dashboard">
-          <img src={logo} alt="Logo" className={styles.logo} />
-        </Link>
+      <div className={styles.introcard}>
+        <div className={styles.overlayBox}>
+          <h1 className={styles.title}>
+            Schools, <br/>
+            <span className={styles.gamified}>Gamified.</span>
+          </h1>
+          <Link to="/dashboard">
+            <img src={logo} alt="Logo" className={styles.logo} />
+          </Link>
+        </div>
+      </div>
+      <div className={styles.login}>
+        <div className={styles.login_title}>
+          <div className={styles.text}>Sign in</div>
+          <div className={styles.desc}>To manage your school dashboard</div>
+          <div className={styles.login_form}>
+            <input type="text" placeholder="USERID" />
+            <input type="text" placeholder="PASSWORD" />
+            <Button label="Continue" />
+          </div>
+        </div>
       </div>
     </div>
   );
