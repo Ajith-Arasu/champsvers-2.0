223f5f1a57b8683bb4521613ac03a61d5195bf03
nithyanandhini375 <nithyanandhini375@gmail.com>
2025-11-25 23:36:16 +0530

    login api

--- src/pages/introCard/index.jsx
+++ src/pages/introCard/index.jsx
@@ -1,9 +1,38 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './style.module.css';
 import logo from '../../assets/images/cvlogo.png';
 import Button from '../../components/Button/Button';
-
 const IntroCard = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("https://5hxz4ksy26.execute-api.ap-south-1.amazonaws.com/dev/api/v1/user/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: username,
+            password: password
+          })
+        }
+      );
+      const data = await res.json();
+      console.log("Response:", data);
+      if (!res.ok) {
+        alert(data.message || "Login Failed");
+        return;
+      }
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Network error");
+    }
+  };
   return (
     <div className={styles.background}>
       <div className={styles.introcard}>
@@ -21,11 +50,19 @@ const IntroCard = () => {
         <div className={styles.login_title}>
           <div className={styles.text}>Sign in</div>
           <div className={styles.desc}>To manage your school dashboard</div>
-          <div className={styles.login_form}>
-            <input type="text" placeholder="USERID" />
-            <input type="text" placeholder="PASSWORD" />
-            <Button label="Continue" />
-          </div>
+          <form className={styles.login_form} onSubmit={loginUser}>
+            <input 
+              type="text"
+              placeholder="USERID"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+            <input
+              type="password"
+              placeholder="PASSWORD"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <Button type="submit" label="Continue" />
+          </form>
         </div>
       </div>
     </div>
