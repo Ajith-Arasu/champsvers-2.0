cb502941990b275eac194f0a51c1a191981c096f
nithyanandhini375 <nithyanandhini375@gmail.com>
2026-01-06 19:43:48 +0530

    API integrated

--- src/pages/Clans/index.jsx
+++ src/pages/Clans/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import styles from './style.module.css';
 import ClanCard from '../../components/Card/ClanCard';
-import img1 from '../../assets/images/scapanthers.png';
-import img2 from '../../assets/images/scajaquars.png';
-import img3 from '../../assets/images/scaleopards.png';
-import img4 from '../../assets/images/scacheetah.png';
 import AddButton from '../../components/Addbutton/AddButton';
 import { useNavigate } from 'react-router-dom';
 import PageHeader from '../../components/Header/PageHeader';
+import axios from 'axios';
 
 const Clans = () => {
-  const clanCards=[
+  const [clanCards, setClanCards]=useState([]);
+  const BASE_URL = import.meta.env.VITE_IMAGE_CDN
+ {/* const clanCards=[
     {id:1, image: img1, label:"SCA PANTHERS", value:"302"},
     {id:2, image:img2, label:"SCA JAGUARS", value:"310"},
     {id:3, image:img3, label:"SCA LEOPARDS", value:"299"},
     {id:4, image:img4, label:"SCA CHEETAH", value:"315"}
-  ];
+  ];*/}
   const navigate=useNavigate();
   const handleImageClick=(id)=>{
     navigate(`/clans/${id}/details`);
@@ -23,13 +23,45 @@ const Clans = () => {
   const handleClick=(id)=>{
     navigate(`/clans/${id}/addclan`);
   }
-  return (
+   const fetchClans = async () => {
+    try {
+      const accessToken = localStorage.getItem("access_token");
+      if (!accessToken) {
+        console.error("Access token not found");
+        return;
+}
+      const api = axios.create({
+        baseURL: "https://5hxz4ksy26.execute-api.ap-south-1.amazonaws.com/dev",
+});
+      const response = await api.get("/api/v1/clans", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+         count: 4,
+          
+        },
+      });
+      console.log("API response:", response.data.data);
+      setClanCards(response.data.data);
+    } catch (error) {
+      console.error("Error fetching clans:", error);
+    }
+};
+
+  useEffect(()=>{
+    fetchClans();
+   },[]);
+
+   console.log(BASE_URL);
+
+ return (
     <div className={styles.clanspage}>
       <PageHeader title="CLANS" dividerwidth="100%">
         <AddButton text="ADD NEW CLAN" onClick={()=>{handleClick(123)}} />
       </PageHeader>
       <div className={styles.clanscard}>
-        {clanCards.map((item, id)=>(<ClanCard key={id} image={item.image} onImageClick={()=>{handleImageClick(item.id)}} label={item.label} value={item.value} />))}
+        {clanCards.map((item, id)=>(<ClanCard key={id} image={`${BASE_URL}${item?.created_by}/PAGES/CLANS/IMAGES/medium/${item?.clan_cover?.name}`} onImageClick={()=>{handleImageClick(item.id)}} label={item.clan_name} value={item.clan_members_count} />))}
       </div>
     </div>
   );
--- src/pages/Quests/index.jsx
+++ src/pages/Quests/index.jsx
@@ -1,32 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './style.module.css';
 import QuestCard from '../../components/QuestCard/QuestCard';
 import AddButton from '../../components/Addbutton/AddButton';
-import { useNavigate } from 'react-router-dom';
 import PageHeader from '../../components/Header/PageHeader';
+import axios from 'axios';
+
 
 const Quests = () =>{ 
+  const [quests, setQuests]= useState([]);
   const navigate=useNavigate();
   const handleClick=(id)=>{
     navigate(`/quests/${id}/questcreation`);
   }
+ {/*const quests = Array.from({ length: 10 }, (_, index) => ({
+  id: index + 1,
+  image: questimg,
+  titleLine1: "A BEAUTIFUL",
+  titleLine2: "AQUARIUM",
+  description: "Draw a fish tank or bowl that you own or wish to own and explain about the breeds of the fishes inside. What food would you feed them? What plants will you plant inside, what other things you will keep inside the  to make it a home for all the fishes. " 
+}));*/}
+   const fetchQuests = async () => {
+    try {
+      const accessToken = localStorage.getItem("access_token");
+      if (!accessToken) {
+        console.error("Access token not found");
+        return;
+}
+      const api = axios.create({
+        baseURL: "https://5hxz4ksy26.execute-api.ap-south-1.amazonaws.com/dev",
+});
+      const response = await api.get("/api/v1/contests", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          contest_type: "MICRO_CONTEST",
+        
+          },
+
+      });
+      console.log("API response:", response.data.data);
+      setQuests(response.data.data);
+    } catch (error) {
+      console.error("Error fetching clans:", error);
+    }
+};
+
+  useEffect(()=>{
+    fetchQuests();
+   },[]);
+
+
   return(
     <div className={styles.questspage}>
       <PageHeader title="QUESTS" dividerwidth="100%">
-       <AddButton text="ADD NEW QUEST" onClick={()=>{handleClick(123)}} />
+        <AddButton text="ADD NEW QUEST" onClick={()=>{handleClick(123)}} />
       </PageHeader>
       <div className={styles.quest_gallery}>
-        <QuestCard />    
-        <QuestCard />
-        <QuestCard />
-        <QuestCard />
-        <QuestCard /> 
-        <QuestCard />    
-        <QuestCard />
-        <QuestCard />
-        <QuestCard />
-        <QuestCard /> 
-      </div>
+        {quests.length > 0 &&
+    Array.from({ length: 10 }).map((_, index) => {
+      const quest = quests[index % quests.length];
+
+      return (
+          <QuestCard
+            key={quest.id}
+            image="https://d1wlhv1hqb6088.cloudfront.net/0798c554-a13a-412f-8143-33ac804cf088/PAGES/MICRO_CONTESTS/IMAGES/medium/questImgNew.jpg"
+            titleLine1={quest.category}
+            titleLine2={quest.title}
+            description={quest.description}
+    />
+  );
+    })}
+</div>
     </div>
   );
  };
